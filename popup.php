8cf4dddbd7b5b6a2d459df0f1dad27cecb48e928
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-01-01 17:21:10 +0100

    Add initial structure, html and styles

---
+++ popup.php
@@ -0,0 +1,8 @@
+<section class="cookie-popup">
+        <div class="cookie-popup__content">
+            <p class="cookie-popup__text">
+            W celu świadczenia usług na najwyższym poziomie stosujemy pliki cookies. Korzystanie z naszej witryny oznacza, że będą one zamieszczane w Państwa urządzeniu. W każdym momencie można dokonać zmiany ustawień Państwa przeglądarki. Zobacz politykę cookies.
+            </p>
+            <button class="cookie-popup__button">Zamknij</button>
+        </div>
+</section>
---
+++ wpblog-cookie-popup.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name: WP Cookie Popup Creator
+ * Description: Simple cookie popup plugin for Wordpress pages
+ * Version: 0.1.0
+ * Author: Arkadiusz Kulewicz
+ * License: GPL v2.0 or later
+ */
+
+
+function wpblog_popup_add_css() {
+    $styles = file_get_contents(plugin_dir_path(__FILE__) . '/' .'style.css');
+    echo "<style>$styles</style>";
+}
+
+function wpblog_add_cookie_popup() {
+    $popup = include(plugin_dir_path(__FILE__) . '/' .'popup.php');
+    echo $popup;
+}
+
+add_action('wp_head', 'wpblog_popup_add_css');
+add_action('wp_footer', 'wpblog_add_cookie_popup');
