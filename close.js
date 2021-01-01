da942b858ded1146a1b0bc26c44cbc62f068b485
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-01-01 18:25:15 +0100

    Add close popup script

---
+++ close.js
@@ -0,0 +1,5 @@
+const popup = document.querySelector('.cookie-popup');
+const button = document.querySelector('.cookie-popup__button');
+button.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
--- wpblog-cookie-popup.php
+++ wpblog-cookie-popup.php
@@ -3,21 +3,26 @@
 /**
  * Plugin Name: WP Cookie Popup Creator
  * Description: Simple cookie popup plugin for Wordpress pages
- * Version: 0.1.1
+ * Version: 0.2.0
  * Author: Arkadiusz Kulewicz
  * License: GPL v2.0 or later
  */
 
+function wpblog_add_cookie_popup() {
+    $popup = file_get_contents(plugin_dir_path(__FILE__) . '/' .'popup.html');
+    echo $popup;
+}
 
 function wpblog_popup_add_css() {
     $styles = file_get_contents(plugin_dir_path(__FILE__) . '/' .'style.css');
     echo "<style>$styles</style>";
 }
 
-function wpblog_add_cookie_popup() {
-    $popup = include(plugin_dir_path(__FILE__) . '/' .'popup.php');
-    echo $popup;
+function wpblog_close_cookie_popup() {
+    $js = file_get_contents(plugin_dir_path(__FILE__) . '/' . 'close.js');
+    echo "<script>$js</script>";
 }
 
 add_action('wp_head', 'wpblog_popup_add_css');
 add_action('wp_footer', 'wpblog_add_cookie_popup');
+add_action('wp_footer', 'wpblog_close_cookie_popup');
