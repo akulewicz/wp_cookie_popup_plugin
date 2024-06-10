59dc6bef913e8d7324427d0df209c3470f20ea2b
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2024-06-10 09:45:49 +0200

    Change button styles

--- popup.php
+++ popup.php
@@ -6,7 +6,7 @@
             <p class="cookie-popup__text">
             W celu świadczenia usług na najwyższym poziomie stosujemy pliki cookies. Korzystanie z naszej witryny oznacza, że będą one zamieszczane w Twoim urządzeniu. W każdym momencie można dokonać zmiany ustawień Twojej przeglądarki. Zobacz politykę cookies.
             </p>
-            <button class="cookie-popup__button button button--blue-border ">Zamknij</button>
-            <a class="button button--blue" href="<?php echo site_url('/polityka-prywatnosci/'); ?>">Polityka prywatności</a>
+            <button class="cookie-popup__button">Zamknij</button>
+            <a class="cookie-popup__button" href="<?php echo site_url('/polityka-prywatnosci/'); ?>">Polityka prywatności</a>
         </div>
 </section>
